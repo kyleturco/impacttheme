922bfe32cd40d6c5093733a9d1b1e2d58dd1029b
Kyle Turco <kyleturco@gmail.com>
2016-03-22 20:53:23 -0500

    start styles and content for homepage template

--- page-templates/page-homepage.php
+++ page-templates/page-homepage.php
@@ -8,7 +8,29 @@ Template Name: Homepage
 
 <div id="home-hero-section">
   <div class="container">
-    <h3>Hello!</h3>
+    <div id="home-hero-content" class="home-hero-width">
+      <h1 class="text-white">Slogan or value statement goes here</h1>
+      <p class="text-white">
+        Collide Media Group is a Nashville-based marketing company that specializes in faith based,
+        family entertainment
+      </p>
+      <div id="home-hero-btn" class="btn-primary">our services</div>
+    </div>
+  </div>
+</div>
+<div id="home-services-section">
+  <div class="container">
+    <div id="home-services-content">
+      <div class="services-box col-md-4">
+        <h2>Campaign Management</h2>
+      </div>
+      <div class="services-box col-md-4">
+        <h2>Brand Development</h2>
+      </div>
+      <div class="services-box col-md-4">
+        <h2>Strategic Partnerships</h2>
+      </div>
+    </div>
   </div>
 </div>
 
