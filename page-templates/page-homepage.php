4e50350c67e904a7a080d63182e997846055284d
Kyle Turco <kyleturco@gmail.com>
2016-03-23 22:22:40 -0500

    add about section to home template

--- page-templates/page-homepage.php
+++ page-templates/page-homepage.php
@@ -21,14 +21,74 @@ Template Name: Homepage
 <div id="home-services-section">
   <div class="container">
     <div id="home-services-content">
-      <div class="services-box col-md-4">
-        <h2>Campaign Management</h2>
+      <div class="services-boxes">
+        <div class="services-box col-md-4">
+          <h2>Campaign Management</h2>
+        </div>
+        <div class="services-box col-md-4">
+          <h2>Brand Development</h2>
+        </div>
+        <div class="services-box col-md-4">
+          <h2>Strategic Partnerships</h2>
+        </div>
       </div>
-      <div class="services-box col-md-4">
-        <h2>Brand Development</h2>
+      <div class="clearfix"></div>
+      <div class="services-content">
+        <div class="campaign-mgmt-content">
+          <p>
+            Our exploration process starts by going back to the beginning.  By taking your brand apart and putting it
+            back together, we are able to set aside things that are not essential to your goals and mission.  In the
+            case of brand development, we are unique in three ways:
+          </p>
+          <div class="col-md-4 quality-item">
+            <h4>Our Faith</h4>
+            <p>
+              We believe that we have a Creator and everything was created, likewise, we look at someone with an
+              idea (or tons of ideas) or a brand, and know this came out of their life somewhere.
+            </p>
+          </div>
+          <div class="col-md-4 quality-item">
+            <h4>Our Experience</h4>
+            <p>
+              Collide has a long history of campaign management on behalf of ministries and brands.  Collide team
+              members have developed brands and quarterbacked campaigns.
+            </p>
+          </div>
+          <div class="col-md-4 quality-item">
+            <h4>Our Experience</h4>
+            <p>
+              A lot of what we do is create a differentiation, which comes with a deep understanding and working
+              knowledge of the market landscape.
+            </p>
+          </div>
+          <div class="clearfix"></div>
+        </div>
       </div>
-      <div class="services-box col-md-4">
-        <h2>Strategic Partnerships</h2>
+    </div>
+  </div>
+</div>
+<div id="about-section">
+  <div class="container">
+    <div class="about-section-content">
+      <h1 class="text-white header-underline">About</h1>
+      <p class="text-white">
+        <b>Collide Media Group is a marketing agency whose mission is to elevate media that inspires a
+        deeper relationship with Christ.</b>
+      </p>
+      <div class="col-md-6 text-white">
+        <p>
+          Over the years, Collide team members have helmed major brand development campaigns for clients such as
+          Thomas Nelson Bibles, Moody Publishing and The Five Love Languages and faith financial icon Ron Blue. They
+          have run and contributed to major theatrical campaigns for over 30 films including The Chronicles of Narnia
+          "The Lion, The Witch, and the Wardrobe" in 2005,
+        </p>
+      </div>
+      <div class="col-md-6 text-white">
+        <p>
+          “Courageous" and "Soul Surfer" in 2011 and Paramount's "Captive" in 2015.  Strategic partnerships have been
+          formed and developed for clients including the recent box office hit "God's Not Dead" and currently with
+          Phil Vischer's "JellyTelly" media platform for families.
+        </p>
       </div>
     </div>
   </div>
